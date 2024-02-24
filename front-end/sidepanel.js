187ccca0be94768ac367cdef728afd9879b9641b
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2024-02-24 14:18:42 +0900

    アイコンの表示

--- front-end/sidepanel.js
+++ front-end/sidepanel.js
@@ -2,7 +2,7 @@
 function faviconURL(u) {
   const url = new URL(chrome.runtime.getURL("/_favicon/"));
   url.searchParams.set("pageUrl", u);
-  url.searchParams.set("size", "16");
+  url.searchParams.set("size", "64");
   return url.toString();
 }
 
